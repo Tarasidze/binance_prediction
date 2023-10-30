526da91f23bcb6f7b5415906863f447c003d6290
Tarasidze <dtar84@ukr.net>
2023-10-30 11:36:29 +0200

    add cci indicator

---
+++ source/cci_indicator.py
@@ -0,0 +1,60 @@
+import os
+import pandas as pd
+import talib as ta
+import matplotlib.pyplot as plt
+from source.settings import Settings
+
+
+class CCIInicator:
+    def __init__(self) -> None:
+        settings = Settings()
+
+        self.period = settings.cci_period
+        self.file_name = settings.data_file
+        self.catalogue = settings.outputs_catalogue
+
+    def get_data_from_file(self, file_name: str = None) -> pd.DataFrame | None:
+        if file_name is None:
+            file_name = self.file_name
+
+        file_path = os.path.join(os.getcwd(), self.catalogue, file_name)
+
+        try:
+            df = pd.read_csv(file_path)
+
+        except FileNotFoundError as exc:
+            print("File not found: ", exc)
+            return None
+
+        return df
+
+    def calculate_cci(self, df: pd.DataFrame = None) -> pd.DataFrame:
+
+        """
+        Calculate CCI-indicator using talib library
+
+        Args:
+            df (pd.DataFrame, optional): data-frame should
+            contain "high", "low", "close" series
+
+        Returns:
+            pd.DataFrame: data-frame with cci-indicator series
+        """
+
+        if df is None:
+            df = self.get_data_from_file()
+
+        df["cci"] = ta.CCI(
+            df["high"], df["low"], df["close"], self.period
+        )
+
+        return df
+
+    def show_plot(self, df: pd.DataFrame) -> None:
+        _, ax = plt.subplots(2, sharex=True)
+        ax[0].plot(df["close"])
+        ax[0].legend(loc="upper left")
+        ax[1].plot(df["cci"])
+        ax[1].legend(loc="upper left")
+        plt.suptitle("__--__sdasd__--__")
+        plt.show()
