245689827e7243796244a2d71b21a92804293903
Tarasidze <dtar84@ukr.net>
2023-10-31 18:58:58 +0200

    rewrite logic

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 import json
+import pandas as pd
 from source.binance_api_manager import BinanceApiManager
 from source.rsi_indicator import RsiLbIndicator
 from source.cci_indicator import CCIInicator
-
 from backtester.backtester import Backtester
+from source.report_to_pdf import create_pdf
 
 
 def main():
     am = BinanceApiManager()
 
-    df = am.get_klines_from_api()
+    # df = am.get_klines_from_api()
+    # am.seve_data_to_file(df=df)
+
+    df = am.get_data_from_file()
+
+    # rsi_up_band, rsi_down_band = RsiLbIndicator().calculate_rsi_bands_lb(df)
+    # cci_indicator = CCIInicator().calculate_cci(df)
+
+    # df["rsi_up"] = rsi_up_band
+    # df["rsi_down"] = rsi_down_band
+    # df["cci"] = cci_indicator
+
+    # df.dropna(inplace=True)
+    # df.reset_index(drop=True, inplace=True)
 
     # am.seve_data_to_file(df=df)
-    # df = am.get_data_from_file()
-
-    rsi_up_band, rsi_down_band = RsiLbIndicator().calculate_rsi_bands_lb(df)
-    df["rsi_up"] = rsi_up_band
-    df["rsi_down"] = rsi_down_band
-    df = CCIInicator().calculate_cci(df)
-    df.dropna(inplace=True)
-    df.reset_index(drop=True, inplace=True)
-
-    am.seve_data_to_file(df=df)
 
     data_dict = Backtester().run_tester(df)
-
-    json.dump(data_dict, open("result.json", "w"))
-
+    json.dump(data_dict, open("source/result.json", "w"))
     print(data_dict)
+
+    create_pdf(data_dict, "outputs/report.pdf")
+
+    # cci = CCIInicator().show_plot(df)
+
+    # am.show_charts(df)
 
 
 if __name__ == "__main__":
