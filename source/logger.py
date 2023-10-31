61b86412212746d1ccdaedbf844f9cdc844470d6
Tarasidze <dtar84@ukr.net>
2023-10-31 09:41:13 +0200

    add loger

--- source/logger.py
+++ source/logger.py
@@ -1,9 +1,14 @@
 import os
+import csv
 import logging
 from source.settings import Settings
 
 
-class Logger:
+class LoggHandler:
+    pass
+
+
+class LoggerLog:
     def __init__(self) -> None:
         settings = Settings()
 
@@ -12,6 +17,45 @@ class Logger:
 
         self.file = os.path.join(os.getcwd(), self.catalog, self.file_name)
 
-    def logg_info_message(self, message):
+    def logg_info_message(self, message: str) -> None:
         logging.basicConfig(filename=self.file, level=logging.INFO)
         logging.info(message)
+
+
+class LoggerCsv:
+    def __init__(self) -> None:
+        settings = Settings()
+
+        self.file_name = settings.loger_file
+        self.catalog = settings.outputs_catalogue
+        self.file_path = os.path.join(
+            os.getcwd(),
+            self.catalog,
+            self.file_name
+        )
+
+    def logg_info_message(self, message: dict, fields: list = None) -> None:
+        fields = [
+            "open_time",
+            "open_price",
+            "close_time",
+            "close_price",
+            "trade_type",
+            "profit"
+        ]
+
+        file_exists = os.path.exists(self.file_path)
+
+        with open(
+            self.file_path,
+            "a+",
+            newline="",
+            encoding="utf-8"
+        ) as csv_file:
+
+            writer = csv.DictWriter(csv_file, fieldnames=fields)
+
+            if not file_exists:
+                writer.writeheader()
+
+            writer.writerow(message)
